b95ba94065ced0e9a1d208c0f29f2095af092f63
Vinicius Almeida <vini@DESKTOP-04PS4U5>
2024-08-13 10:33:27 -0300

    teste3 alterado

--- test/test3.js
+++ test/test3.js
@@ -1,4 +1,4 @@
 // Teste 3, aprendendo a mexer no gitlens asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf afda
 // bbbbb
 
-// teste3 para verificar o git reset
+// teste3 para verificar o git reset  E AQUI SEGUEM AS MODIFICAÇÕES NO ARQUIVO TEST3
